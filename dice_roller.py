18c1c6ed2cb10d01760346573758b2a01bce414e
PavelBodle <paveliitb@gmail.com>
2020-10-19 23:55:05 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -3,3 +3,20 @@ def main():
 
 if __name__== "__main__":
   main()
+
+
+list = [18,5,7,6,9,2,4,1,0,15,59,55,100,11,32,100,69,81]
+
+def sort(list):
+    for i in range(len(list)):
+        minpos = i
+        for j in range(i, len(list)):
+            if list[j] > list[minpos]:
+                minpos = j
+        temp = list[i]
+        list[i] = list[minpos]
+        list[minpos] = temp
+        print(list)  # To see every swap
+
+sort(list)
+print("Sorting by Selection Sort:", list)
